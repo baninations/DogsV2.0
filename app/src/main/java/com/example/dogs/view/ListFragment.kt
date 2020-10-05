9bb16d28bdd1280f68cf354294dcae7c2480d69a
Alban <baninations@gmail.com>
2020-10-05 21:15:36 +0200

    Fragments with arguments

---
+++ app/src/main/java/com/example/dogs/view/ListFragment.kt
@@ -0,0 +1,38 @@
+package com.example.dogs.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.dogs.R
+import kotlinx.android.synthetic.main.fragment_list.*
+
+
+class ListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnDetail.setOnClickListener {
+            val action = ListFragmentDirections.actionDetailFragment()
+            action.dogUuid = 278
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
